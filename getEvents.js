9b6a21ce28a24524eaa7592220f66c61e84a85ad
sparksflyupwards <saad.jamil.khan@gmail.com>
2021-01-21 22:28:51 -0500

    got all the event links

--- getEvents.js
+++ getEvents.js
@@ -1,7 +1,12 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 const { table } = require('console');
-const url = 'http://www.ufcstats.com/statistics/events/completed?page=1';
+
+ufc_events = [];
+
+for(let i = 1; i<21; i++){
+
+    const url = 'http://www.ufcstats.com/statistics/events/completed?page='+i;
 
 
 rp(url)
@@ -14,19 +19,30 @@ rp(url)
         //lets put it into an array now;
     //data = data.split(' ');
     //console.log(data);
-    
-    table_body = $('body > section > div > div > div > div.b-statistics__sub-inner > div > table > tbody > tr').each(function(i,elem) {
-   
-        table_row = $(this).text();
+    /**
+     * to get the links
+     * body > section > div > div > div > div.b-statistics__sub-inner > div > table > tbody > tr:nth-child(3) > td:nth-child(1) > i > a
+     * 
+     * to get the whole table row
+     * body > section > div > div > div > div.b-statistics__sub-inner > div > table > tbody > tr
+     */
 
+    
+    table_body = $('body > section > div > div > div > div.b-statistics__sub-inner > div > table > tbody > tr > td > i > a').each(function(i,elem) {
+   
+        event_row = $(this).text();
+        var link = $(elem).attr('href');
+        console.log(link)
+        /** 
         //lets clean the data
         //remove extra spaces
     
-        table_row = table_row.replace(/\s\s+/g, ' ');
+        event_row = event_row.replace(/\s\s+/g, ' ');
         //lets put it into an array now;
-        table_row_array = table_row.split(' ');
-        
-        console.log(table_row)
+        event_row_array = event_row.split(' ');
+        */
+        ufc_events.push(link)
+
     })
     
     
@@ -35,3 +51,5 @@ rp(url)
   .catch(function(err){
     //handle error
   });
+}
+console.log(ufc_events)
