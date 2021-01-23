64d2d78485adce2604d948d6144ca7622e8a6bee
sparksflyupwards <saad.jamil.khan@gmail.com>
2021-01-22 20:08:09 -0500

    the object is properly formatted and entered for one event

--- scrape.js
+++ scrape.js
@@ -50,45 +50,30 @@ rp(url)
         table_row = $(this).text();
         //lets clean the data
         //remove extra spaces
+        table_row_array  = table_row.split("\n").filter((str)=>{return /\S/.test(str)});
+        table_row_array=table_row_array.map((str)=>{ return str.trim()});
         
-        console.log("SOME " +table_row.split("\n").filter((str)=>{return /\S/.test(str)}))
-        table_row = table_row.replace(/\s\s+/g, ' ');
-        
-        //lets put it into an array now;
-        table_row_array = table_row.split(' ');
-        
-       
-        padding = 0; 
-            let fight_result = table_row_array[1];
-            let fighter_a_name = table_row_array[2]+ + " "+ table_row_array[3];
-            let fighter_b_name = table_row_array[4]+ " "+ table_row_array[5] 
-            
-            //console.log(fighter_b_name)
-            console.log(table_row_array)
         fight = {
-            result: table_row_array[1],
-            fighter_a:table_row_array[2]+ " "+ table_row_array[3],
-            fighter_b: table_row_array[4]+ " "+ table_row_array[5] ,
-            knock_downs_fighter_a: table_row_array[6 + padding],
-            knock_downs_fighter_b: table_row_array[7 + padding],
-            significant_strikes_fighter_a: table_row_array[8 + padding],
-            significant_strikes_fighter_b: table_row_array[9 + padding],
-            take_downs_fighter_a: table_row_array[10 + padding],
-            take_downs_fighter_b: table_row_array[11 + padding],
-            submission_attempts_fighter_a: table_row_array[12 + padding],
-            submission_attempts_fighter_b: table_row_array[13 + padding],
-            weight_class: table_row_array[14 + padding],
-            result_method: table_row_array[15 + padding],
-            method_detail: isNumeric(table_row_array[16 + padding]) ? '' : table_row_array.slice(16,table_row_array.length-3),
-            total_rounds: table_row_array[table_row_array.length-3],
-            end_time: table_row_array[table_row_array.length-2]
-
-
+            result: table_row_array[0],
+            fighter_a:table_row_array[1],
+            fighter_b: table_row_array[2] ,
+            knock_downs_fighter_a: table_row_array[3],
+            knock_downs_fighter_b: table_row_array[4],
+            significant_strikes_fighter_a: table_row_array[5],
+            significant_strikes_fighter_b: table_row_array[6],
+            take_downs_fighter_a: table_row_array[7],
+            take_downs_fighter_b: table_row_array[8],
+            submission_attempts_fighter_a: table_row_array[9],
+            submission_attempts_fighter_b: table_row_array[10],
+            weight_class: table_row_array[11],
+            result_method: table_row_array[12],
+            method_detail: isNumeric(table_row_array[13]) ? '' : table_row_array[14],
+            total_rounds: table_row_array[table_row_array.length-2],
+            end_time: table_row_array[table_row_array.length-1]
 
             }
 
            
-
             fights.push(fight);
       });
     
@@ -104,9 +89,12 @@ rp(url)
       fs.appendFileSync('fights'+counter+'.json', "{ \"event_title\": \"");
       fs.appendFileSync('fights'+counter+'.json', event_title);
       fs.appendFileSync('fights'+counter+'.json', "\", \"total_fights\":"+total_fights+", \"fights\" :[\n");
-      
+      let i =0;
       for(some_fight of fights){
+        
         fs.appendFileSync('fights'+counter+'.json', JSON.stringify(some_fight));
+        i++;
+        if(i<total_fights)
         fs.appendFileSync('fights'+counter+'.json', ",\n");
   
       }
