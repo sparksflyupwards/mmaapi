0c53647574b9458941b80752779291b0a182d788
sparksflyupwards <saad.jamil.khan@gmail.com>
2021-01-22 19:44:11 -0500

    reading by splitting new lines fixes a lot of name reading issues

--- scrape.js
+++ scrape.js
@@ -50,28 +50,36 @@ rp(url)
         table_row = $(this).text();
         //lets clean the data
         //remove extra spaces
-    
+        
+        console.log("SOME " +table_row.split("\n").filter((str)=>{return /\S/.test(str)}))
         table_row = table_row.replace(/\s\s+/g, ' ');
+        
         //lets put it into an array now;
         table_row_array = table_row.split(' ');
         
        
-
+        padding = 0; 
+            let fight_result = table_row_array[1];
+            let fighter_a_name = table_row_array[2]+ + " "+ table_row_array[3];
+            let fighter_b_name = table_row_array[4]+ " "+ table_row_array[5] 
+            
+            //console.log(fighter_b_name)
+            console.log(table_row_array)
         fight = {
             result: table_row_array[1],
             fighter_a:table_row_array[2]+ " "+ table_row_array[3],
-            fighter_b: table_row_array[4]+ " "+ table_row_array[5],
-            knock_downs_fighter_a: table_row_array[6],
-            knock_downs_fighter_b: table_row_array[7],
-            significant_strikes_fighter_a: table_row_array[8],
-            significant_strikes_fighter_b: table_row_array[9],
-            take_downs_fighter_a: table_row_array[10],
-            take_downs_fighter_b: table_row_array[11],
-            submission_attempts_fighter_a: table_row_array[12],
-            submission_attempts_fighter_b: table_row_array[13],
-            weight_class: table_row_array[14],
-            result_method: table_row_array[15],
-            method_detail: isNumeric(table_row_array[16]) ? '' : table_row_array.slice(16,table_row_array.length-3),
+            fighter_b: table_row_array[4]+ " "+ table_row_array[5] ,
+            knock_downs_fighter_a: table_row_array[6 + padding],
+            knock_downs_fighter_b: table_row_array[7 + padding],
+            significant_strikes_fighter_a: table_row_array[8 + padding],
+            significant_strikes_fighter_b: table_row_array[9 + padding],
+            take_downs_fighter_a: table_row_array[10 + padding],
+            take_downs_fighter_b: table_row_array[11 + padding],
+            submission_attempts_fighter_a: table_row_array[12 + padding],
+            submission_attempts_fighter_b: table_row_array[13 + padding],
+            weight_class: table_row_array[14 + padding],
+            result_method: table_row_array[15 + padding],
+            method_detail: isNumeric(table_row_array[16 + padding]) ? '' : table_row_array.slice(16,table_row_array.length-3),
             total_rounds: table_row_array[table_row_array.length-3],
             end_time: table_row_array[table_row_array.length-2]
 
