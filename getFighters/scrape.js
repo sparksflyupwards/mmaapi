49a84d7e5e34d213f25afb5af2da22bb31da0457
sparksflyupwards <saad.jamil.khan@gmail.com>
2022-01-01 02:15:06 -0500

    now scrapes for every letter until pages run out

--- getFighters/scrape.js
+++ getFighters/scrape.js
@@ -9,60 +9,71 @@ const { resolve } = require('path');
 
 
 const scrapeAllFighers = async ()=>{
-    const letters = "A"
-    //BCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     const baseLink = "http://ufcstats.com/statistics/fighters?char="
 
     for(letter of letters){
-        await axios.get(baseLink+letter).then((response)=>{
- 
 
-            $ = cheerio.load(response.data);
-            //parses through each table cell
-            table_body =  $('body > section > div > div > div > table > tbody > tr').each((i, elem) => {
-
-           
-            
-                //format the elements data into an array slicing out the spaces
-                table_row = $(elem).text();
-                table_row_array  = table_row.split("\n").filter((str)=>{return /\S/.test(str)});
-                table_row_array=table_row_array.map((str)=>{ return str.trim()});
+        let count = 1;
+        let stayOnLetter = true;
+        while(stayOnLetter){
+            let url = baseLink+letter+"&page=" + count;
+            //console.log(url)
+            count++
+            await axios.get(url).then((response)=>{
  
-                //find link for more figher info
-                let link = $(elem).children('.b-statistics__table-col').children().attr('href');
-                  
+                $ = cheerio.load(response.data);
+                //parses through each table cell if there are cells on this page else ends scraping of the letter
+                const table_row_selector = 'body > section > div > div > div > table > tbody > tr'
+                if($(table_row_selector).length <= 1){
+                    stayOnLetter = false;
+                    console.log("For letter: "+ letter + " finished with num of pages: " + (count - 2 ))
+                }
+                table_body =  $(table_row_selector).each((i, elem) => {
+                   
+               
                 
-                //if link undefined then the row is not a figher
-                let fighter_id = "";
-                if(link){
-                    fighter_id =  link.slice(36,)
-                }
-                else {
-                    return;
-                }
+                    //format the elements data into an array slicing out the spaces
+                    table_row = $(elem).text();
+                    table_row_array  = table_row.split("\n").filter((str)=>{return /\S/.test(str)});
+                    table_row_array=table_row_array.map((str)=>{ return str.trim()});
+     
+                    //find link for more figher info
+                    let link = $(elem).children('.b-statistics__table-col').children().attr('href');
+                      
                     
-                  
-                //formulate the figher into an object
-                let fighter = {
-                    name: table_row_array[0] + " " + table_row_array[1],
-                    nick_name: table_row_array[2],
-                    height: table_row_array[3],
-                    weight: table_row_array[4],
-                    reach: table_row_array[5],
-                    stance: table_row_array[6],
-                    total_wins: table_row_array[7],
-                    total_losses: table_row_array[8],
-                    total_draws: table_row_array[9],
-                    fighter_id: fighter_id,
-                    link: link
-
-                }
-                
-                console.log(fighter)
-                return
+                    //if link undefined then the row is not a figher
+                    let fighter_id = "";
+                    if(link){
+                        fighter_id =  link.slice(36,)
+                    }
+                    else {
+                        return;
+                    }
+                        
+                    //formulate the figher into an object
+                    let fighter = {
+                        name: table_row_array[0] + " " + table_row_array[1],
+                        nick_name: table_row_array[2],
+                        height: table_row_array[3],
+                        weight: table_row_array[4],
+                        reach: table_row_array[5],
+                        stance: table_row_array[6],
+                        total_wins: table_row_array[7],
+                        total_losses: table_row_array[8],
+                        total_draws: table_row_array[9],
+                        fighter_id: fighter_id,
+                        link: link
+    
+                    }
+                    
+                    //console.log(fighter)
+                    return
+                })
+    
             })
-
-        })
+        }
+       
     }
     
 }
